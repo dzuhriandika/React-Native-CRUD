e8ac1ef0f6aee5ab3ea9aae2f56be585164c8434
dzuhriandika <dzuhriandika@gmail.com>
2019-09-02 20:41:13 +0700

    hehe

--- Components/Register.js
+++ Components/Register.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, TextInput} from 'react-native';
-import {Button} from 'native-base';
+import {StyleSheet, Text, View, TextInput,Button} from 'react-native';
 
 export default class Register extends Component{
   static navigationOptions = { header: null};
